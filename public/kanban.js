302c3a77325d43098940eff352c451c9840026f7
naomi-morse <100390944+naomi-morse@users.noreply.github.com>
2022-06-05 15:01:52 +1000

    updated comments for content and kanban css and html

--- public/kanban.js
+++ public/kanban.js
@@ -1,3 +1,5 @@
+// KANBAN JAVASCRIPT
+
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
@@ -28,16 +30,6 @@ function createTask(){
 }
 
 function saveTask(){
-    // var saveButton = document.getElementById("save-button");
-    // var editButton = document.getElementById("edit-button");
-    // if (saveButton.style.display === "none") {
-    //     saveButton.style.display = "block";
-    //     editButton.style.display = "none";
-    // } else{
-    //     saveButton.style.display = "none";
-    //     editButton.style.display = "block";
-    // }
-
     var todo = document.getElementById("todo");
     var taskName = document.getElementById("task-name").value;
     todo.innerHTML += `
@@ -58,5 +50,73 @@ function editTask(){
         editButton.style.display = "block";
     }
 }
-
 // credit to https://karthikdevarticles.com/creating-a-kanban-board-with-html-css-and-javascript#heading-html
+
+
+
+//TASKLIST JS
+//The Document method getElementById() returns an element object representing the element whose id property matches the specified string.
+const form = document.getElementById("taskform");
+const button = document.querySelector("#taskform > button")
+var taskInput = document.getElementById("taskInput");
+var tasklist = document.getElementById("tasklist");
+
+var dueDateInput = document.getElementById("dueDateInput");
+var completionTimeInput = document.getElementById("completionTimeInput");
+var estimatedTimeInput = document.getElementById("estimatedTimeInput");
+var priorityInput = document.getElementById("priorityInput");
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  let task = taskInput.value;
+  let dueDate = dueDateInput.value;
+  let completionTime = completionTimeInput.value;
+  let estimatedTime = estimatedTimeInput.value;
+  let priorityRating = priorityInput.options[priorityInput.selectedIndex].value;
+  addTask(task, dueDate, estimatedTime, priorityRating, completionTime, false);
+  console.log(taskList);
+})
+
+var taskListArray = [];
+
+function addTask(taskDescription, dueDate, estimatedTime, priorityRating, completionTime, completionStatus) {
+  let d = new Date();
+  let dateCreated = d.getFullYear();
+  let task = {
+    taskDescription,
+    dueDate,
+    dateCreated,
+    estimatedTime,
+    completionTime,
+    priorityRating,
+    estimatedTime,
+    completionStatus
+  };
+  taskListArray.push(task);
+  renderTask(task);
+}
+
+function renderTask(task){
+  // Create HTML elements
+  let item = document.createElement("li");
+  item.innerHTML = "<p>" + task.taskDescription + "</p>";
+
+  tasklist.appendChild(item);
+
+  // Extra Task DOM elements
+  let delButton = document.createElement("button");
+  let delButtonText = document.createTextNode("Delete Task");
+  delButton.appendChild(delButtonText);
+  item.appendChild(delButton);
+
+
+  // Event Listeners for DOM elements
+  delButton.addEventListener("click", function(event){
+    event.preventDefault();
+    item.remove();
+  })
+
+
+  // Clear the input form
+  form.reset();
+}
